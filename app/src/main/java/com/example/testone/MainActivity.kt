85fc46b60372b787ac58421a70154138fb38ab63
Navdeep <navdeep@Navdeeps-MacBook-Pro.local>
2020-08-21 10:31:03 -0700

    master 4

--- app/src/main/java/com/example/testone/MainActivity.kt
+++ app/src/main/java/com/example/testone/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        //4
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
