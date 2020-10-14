63be5acd1247f31921eee0159e36a862a4b43c74
Navdeep <navdeep@Navdeeps-MacBook-Pro.local>
2020-10-14 10:50:13 -0700

    Updated fragment view

--- app/src/main/java/com/example/testone/FourthFragment.kt
+++ app/src/main/java/com/example/testone/FourthFragment.kt
@@ -1,6 +1,7 @@
 package com.example.testone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,13 +19,14 @@ class FourthFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_fourth, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        view.findViewById<Button>(R.id.button_fourth).setOnClickListener {
+            Log.e("Button Clicked", "Success")
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
